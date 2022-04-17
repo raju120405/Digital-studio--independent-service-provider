911fac209681df09e165ff9142968ce18deb0db6
raju120405 <raju120405@gmail.com>
2022-04-17 21:24:50 +0600

    data-loded

--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -1,13 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 // import images1 from '../../../images/'
 
 // const services = [
 //     {id:1, name:'wedding', price:'$250',description:'The fruits of a wedding photographers work probably last the longest of any wedding supplier and theyre something all wedding couples treasure for a lifetime.',img: ''}
 // ]
+
+
 const Services = () => {
+    const [ services,setServices]=useState([]);
+    useEffect(()=>{
+        fetch('Fackdata.json')
+        .then(res=>res.json())
+        .then(data=>setServices(data))
+    },[])
     return (
         <div>
-            <h2>this is services</h2>
+            <h2>this is services{services.length}</h2>
         </div>
     );
 };
