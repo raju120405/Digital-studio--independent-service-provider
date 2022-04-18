1a359982de4cef65ac2ba11fabbe7a764cd51aa4
raju120405 <raju120405@gmail.com>
2022-04-18 10:17:24 +0600

    register from added

--- src/Pages/Shared/Login/Register/Register.js
+++ src/Pages/Shared/Login/Register/Register.js
@@ -1,9 +1,35 @@
 import React from 'react';
+import { Button, Form } from 'react-bootstrap';
 
 const Register = () => {
     return (
-        <div>
-            <h2>Please Register</h2>
+        <div className='w-50 mx-auto'>
+            <h2 className='text-primary text-center mt-2'>Please Register</h2>
+            <Form >
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>Your Name</Form.Label>
+                    <Form.Control  type="email" placeholder="Enter Your Name" required />
+                    
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control  type="email" placeholder="Enter email" required />
+                    <Form.Text className="text-muted">
+                        We'll never share your email with anyone else.
+                    </Form.Text>
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control  type="password" placeholder="Password" required />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                    <Form.Check type="checkbox" label="Check me out" />
+                </Form.Group>
+                <Button variant="primary" type="submit">
+                    Submit
+                </Button>
+            </Form>
         </div>
     );
 };
