06047b3ced9c6fd3c37a576d78591f9188266563
raju120405 <raju120405@gmail.com>
2022-04-18 09:54:42 +0600

    login from working

--- src/Pages/Shared/Login/Login.js
+++ src/Pages/Shared/Login/Login.js
@@ -1,14 +1,23 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { Button, Form } from 'react-bootstrap';
 
 const Login = () => {
+    const emailRef= useRef('');
+    const passwordRef = useRef('')
+
+    const handleSubmit= event=>{
+        event.preventDefault();
+        const email= emailRef.current.value;
+        const password = passwordRef.current.value;
+        console.log(email,password)
+    }
     return (
         <div className='w-50 mx-auto mt-2'>
            <h2 className='text-primary text-center'>Please Login</h2> 
-           <Form>
+           <Form onSubmit={handleSubmit}>
   <Form.Group className="mb-3" controlId="formBasicEmail">
     <Form.Label>Email address</Form.Label>
-    <Form.Control type="email" placeholder="Enter email" />
+    <Form.Control ref={emailRef} type="email" placeholder="Enter email" required />
     <Form.Text className="text-muted">
       We'll never share your email with anyone else.
     </Form.Text>
@@ -16,7 +25,7 @@ const Login = () => {
 
   <Form.Group className="mb-3" controlId="formBasicPassword">
     <Form.Label>Password</Form.Label>
-    <Form.Control type="password" placeholder="Password" />
+    <Form.Control ref={passwordRef} type="password" placeholder="Password" required/>
   </Form.Group>
   <Form.Group className="mb-3" controlId="formBasicCheckbox">
     <Form.Check type="checkbox" label="Check me out" />
