53dec345f7690170260bb64e4d98e6fad7f35f8d
raju120405 <raju120405@gmail.com>
2022-04-18 14:23:03 +0600

    about me

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -1,9 +1,20 @@
 import React from 'react';
-
+import myphoto from '../../images/my photo/my photo.jpg'
 const About = () => {
     return (
         <div>
-            this is about me
+            <h3 className='text-center'>About me</h3>
+            <div className='d-flex justify-content-around '>
+                <div><img src={myphoto} alt="" /></div>
+                <div> 
+                    <h2>My Name is Md.Raju Ahmed</h2>
+                    <h5>my goal as a web developer</h5>
+                    <p>I want to be a junior web developer after finishing this corse,<br /> i stricly follow the Programing Hero instraction.I am doing very hard working to acchive a job.</p>
+                    
+
+                </div>
+            </div>
+            
         </div>
     );
 };
--- src/Pages/Blogs/Blogs.js
+++ src/Pages/Blogs/Blogs.js
@@ -2,8 +2,24 @@ import React from 'react';
 
 const Blogs = () => {
     return (
-        <div>
-           <h2>this is blogs</h2> 
+        <div className='container'>
+            <h2 className='text-center m-4'>This is Blog pages</h2>
+            <h4>1.Question = Difference-Between-Authentication-and-Authorization</h4>
+            <h6 className='text-center'>Answer No.1</h6>
+            <p>Authentication <br /> Authentication works through passwords, one-time pins, biometric information, and other information provided or entered by the user.</p>
+            <br />
+            <p>Authorization</p>
+            <p>Authorization works through settings that are implemented and maintained by the organization.</p>
+
+            <h4>2.Question = Why are you using Firebase?</h4>
+            <h6 className='text-center'>Answer No.2</h6>
+            <p>Simple, easy and quick to learn, explore and develop machine learning features for beginners, intermediate and experts
+            </p>
+            <h4>3.Question = What other services des firebase provide other then Authentication?</h4>
+            <h6 className='text-center'>Answer No.3</h6>
+            <p>TeleSign.com...Securely onboard new users and protect account access with two-factor authentication APIs. Helping digital enterprises protect, connect and engage their consumers. Get started today! Reduce Fake accounts. Phone Number Scoring. Talk To An Expert.
+
+            </p>
         </div>
     );
 };
