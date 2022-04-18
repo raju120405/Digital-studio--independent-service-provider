307d513f0c00bf5e3c7beaa2d3f8079837a313be
raju120405 <raju120405@gmail.com>
2022-04-18 11:55:07 +0600

    api

--- src/Pages/Shared/Login/Register/Register.js
+++ src/Pages/Shared/Login/Register/Register.js
@@ -1,8 +1,16 @@
 import React, { useRef } from 'react';
 import { Button, Form } from 'react-bootstrap';
 import { Link, useNavigate } from 'react-router-dom';
-
+import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import auth from '../../../../firebase.init';
 const Register = () => {
+    const [
+        createUserWithEmailAndPassword,
+        user,
+        loading,
+        error,
+      ] = useCreateUserWithEmailAndPassword(auth);
+
     const nameRef= useRef('');
     const emailRef = useRef('');
     const passwordRef = useRef('');
@@ -15,7 +23,8 @@ const Register = () => {
         const name= nameRef.current.value;
         const email = emailRef.current.value;
         const password = passwordRef.current.value;
-        console.log(name,email, password)
+        
+        createUserWithEmailAndPassword(email,password);
     }
     return (
         <div className='w-50 mx-auto'>
