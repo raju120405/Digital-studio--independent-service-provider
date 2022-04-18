818b133466b904f40c405f085d0cca92894664fa
raju120405 <raju120405@gmail.com>
2022-04-18 10:54:28 +0600

    register data found

--- src/Pages/Shared/Login/Register/Register.js
+++ src/Pages/Shared/Login/Register/Register.js
@@ -1,19 +1,34 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { Button, Form } from 'react-bootstrap';
+import { Link, useNavigate } from 'react-router-dom';
 
 const Register = () => {
+    const nameRef= useRef('');
+    const emailRef = useRef('');
+    const passwordRef = useRef('');
+    const navigate=useNavigate();
+    const navigateLogin = ()=>{
+        navigate('/login')
+    }
+    const handleRegister = event =>{
+        event.preventDefault();
+        const name= nameRef.current.value;
+        const email = emailRef.current.value;
+        const password = passwordRef.current.value;
+        console.log(name,email, password)
+    }
     return (
         <div className='w-50 mx-auto'>
             <h2 className='text-primary text-center mt-2'>Please Register</h2>
-            <Form >
+            <Form onSubmit={handleRegister}>
                 <Form.Group className="mb-3" controlId="formBasicEmail">
                     <Form.Label>Your Name</Form.Label>
-                    <Form.Control  type="email" placeholder="Enter Your Name" required />
+                    <Form.Control ref={nameRef} type="text" placeholder="Enter Your Name"  />
                     
                 </Form.Group>
                 <Form.Group className="mb-3" controlId="formBasicEmail">
                     <Form.Label>Email address</Form.Label>
-                    <Form.Control  type="email" placeholder="Enter email" required />
+                    <Form.Control ref={emailRef} type="email" placeholder="Enter email" required />
                     <Form.Text className="text-muted">
                         We'll never share your email with anyone else.
                     </Form.Text>
@@ -21,15 +36,14 @@ const Register = () => {
 
                 <Form.Group className="mb-3" controlId="formBasicPassword">
                     <Form.Label>Password</Form.Label>
-                    <Form.Control  type="password" placeholder="Password" required />
+                    <Form.Control ref={passwordRef} type="password" placeholder="Password" required />
                 </Form.Group>
-                <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                    <Form.Check type="checkbox" label="Check me out" />
-                </Form.Group>
+               
                 <Button variant="primary" type="submit">
                     Submit
                 </Button>
             </Form>
+            <p>Already have an account ? <Link to="/login" className='text-danger pe-auto text-decoration-none' onClick={navigateLogin}>Please Login</Link></p>
         </div>
     );
 };
