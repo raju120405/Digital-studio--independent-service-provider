de6af9f3bf425b7a3b53979fc3c1fdd39c821e90
raju120405 <raju120405@gmail.com>
2022-04-18 10:04:25 +0600

    register button working

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Home from './Pages/Home/Home';
 import Footer from './Pages/Shared/Footer/Footer';
 import Header from './Pages/Shared/Header/Header';
 import Login from './Pages/Shared/Login/Login';
+import Register from './Pages/Shared/Login/Register/Register';
 import NotFound from './Pages/Shared/NotFound/NotFound';
 
 function App() {
@@ -17,6 +18,7 @@ function App() {
         <Route path="/home" element={<Home></Home>}></Route>
         <Route path="/about" element={<About></About>}></Route>
         <Route path="/login" element={<Login></Login>}></Route>
+        <Route path="/register" element={<Register></Register>}></Route>
         
         <Route path="*" element={<NotFound></NotFound>}></Route>
       </Routes>
